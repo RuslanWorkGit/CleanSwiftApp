21bd376493bdfce46a0d72c1eeb3def42bc02846
RuslanWorkGit <lulka758@gmail.com>
2025-04-25 14:56:25 +0300

    Create empty project

---
+++ TestTaskCleanSwift/TestTaskCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskCleanSwift
+//
+//  Created by Ruslan Liulka on 25.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
